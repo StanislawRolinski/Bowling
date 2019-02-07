2f497564e1358660472cbacccc30ffffed477ae8
StanislawRolinski <stasrolinskyi@gmail.com>
2019-02-07 22:11:34 +0100

    0.0.2 reset ball . animation controller

--- Assets/START Pack/Scripts/Ball.cs
+++ Assets/START Pack/Scripts/Ball.cs
@@ -8,14 +8,21 @@ public class Ball : MonoBehaviour
     public Vector3 ballVelocity;
 
     public bool inPlay = false;
+
+    private Vector3 ballStartPos;
     private Rigidbody rigidBody;
     private AudioSource ballSound;
+   
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         rigidBody.useGravity = false;
+        ballStartPos = transform.position;
+        
         }
 
     public void Launch(Vector3 velocity)
@@ -29,6 +36,11 @@ public class Ball : MonoBehaviour
 
     public void Reset()
     {
+        inPlay = false;
+        transform.position = ballStartPos;
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.angularVelocity = Vector3.zero;
+        rigidBody.useGravity = false;
 
     }
 
